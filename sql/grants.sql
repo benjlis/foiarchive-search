1c1b6945bd3fc180c5085ea04ce8588beae1c035
Benjamin Lis <benjaminlis@Benjamins-iMac.local>
2023-11-30 11:09:55 -0500

    Added creation of foiarchive_gui user

--- sql/grants.sql
+++ sql/grants.sql
@@ -1,3 +1,10 @@
+create role foiarchive_gui with login;
+alter role foiarchive_gui password :'pswd';
+alter role foiarchive_gui set search_path to foiarchive,public;
+-- alter role :uni set search_path to default;
+alter role foiarchive_gui connnetion limit 10;
+grant web_anon to foiarchive_gui;
+
 grant select on mosaic.docs_oct73 mosaic_gui;
 grant select on mosaic.classifications to mosaic_gui;
 grant select on mosaic.corpora to mosaic_gui;
