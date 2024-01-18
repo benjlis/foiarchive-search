29a2f74917034744ac4d43fed484e71719abe5ee
Benjamin Lis <benjaminlis@Benjamins-iMac.local>
2024-01-18 11:14:39 -0500

    Removed prints from execute()

--- db.py
+++ db.py
@@ -11,8 +11,6 @@ def load_execute(filename, ttl):
     return conn.query(load(filename), ttl=ttl)
 
 def execute(query):
-    print('query:')
-    print(query)
     return conn.query(query)
 
 @st.cache_data
