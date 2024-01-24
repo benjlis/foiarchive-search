0ae03fe0ac746152debc1d829e5eb33d2b3ab848
Benjamin Lis <benjaminlis@Benjamins-iMac.local>
2024-01-24 11:13:08 -0500

    Added column configurations

--- aggrid.py
+++ aggrid.py
@@ -6,21 +6,23 @@ def grid(df):
     gb.configure_default_column(value=True, editable=False)
     gb.configure_grid_options(domLayout='normal')
     gb.configure_selection(selection_mode='single', groupSelectsChildren=False)
-    # gb.configure_auto_height(False)
     gb.configure_pagination(paginationAutoPageSize=False, 
-                            paginationPageSize=20)
-    # gb.configure_column('email_id', hide=True)
-    # gb.configure_column('pg_number', hide=True)
-    # gb.configure_column('top_topic', hide=True)
-    # gb.configure_column('entities', hide=True)
-    # gb.configure_column('sent', maxWidth=150)
-    # gb.configure_column('subject', maxWidth=600)
-    # gb.configure_column('from', maxWidth=225)
-    # gb.configure_column('to', maxWidth=425)
+                            paginationPageSize=12)
+    # gb.configure_auto_height(False)
+    gb.configure_column('authored', 'Date', width=150)
+    gb.configure_column('corpus', 'Corpus', width=120)
+    gb.configure_column('classification', 'Classification', width=150)
+    gb.configure_column('title','Title', width=500)
+    gb.configure_column('doc_id', 'Doc ID', width=270)
+    gb.configure_column('pg_cnt', 'Pages', width=115)
+    gb.configure_column('word_cnt', 'Words', width=115)
+    gb.configure_column('doc_url', hide=True)
+    gb.configure_column('body', hide=True)
     gridOptions = gb.build()
     grid_response = AgGrid(df, gridOptions=gridOptions,
                            return_mode_values='AS_INPUT',
                            update_mode='SELECTION_CHANGED',
+                           theme='material',
                            allow_unsafe_jscode=False,
                            enable_enterprise_modules=False)    
     return grid_response['selected_rows']
