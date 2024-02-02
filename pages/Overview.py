662ae2e0cbcae64b95682485f8f2c01a66f43477
Benjamin Lis <benjaminlis@Benjamins-iMac.local>
2024-02-01 21:20:05 -0500

    Initial commit

---
+++ pages/Overview.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.header("FOIArchive Overview")
+st.subheader("Motivation")
+st.markdown("Although this video is a few years old, it still does a great job\
+             of why we built and continue to expand the FOIArchive.")
+st.video("https://www.youtube.com/watch?v=6oTNw2jZ3zw&t=42s")
