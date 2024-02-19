4189dc389e24a9d4786a78830d9e18d399596c3c
Benjamin Lis <benjaminlis@Benjamins-iMac.local>
2024-02-19 13:13:08 -0500

    Added page_name argumet to page()

--- configs.py
+++ configs.py
@@ -5,14 +5,14 @@ import streamlit as st
 with open("appconfig.toml", mode="rb") as fp:
     config = tomli.load(fp)
 
-def page():
-    st.set_page_config(page_title=config["tab_title"],
+def page(page_name):
+    st.set_page_config(page_title=f'{config["tab_title"]}',
                        page_icon=config["favicon"],
                        layout="wide",
                        menu_items={'Get Help': config["help_action"],
                                    'Report a bug': config["bug_action"],
                                    'About': f"### {config['gui_title']}\n" +
                                             f"{config['about_body']}"})
-    st.title(config['gui_title'])
+    st.title(f'{config["gui_title"]} - {page_name}')
     with st.sidebar:
         boilerplate.sidebar()
