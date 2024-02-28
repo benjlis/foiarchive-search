0a09d91f4832a61ff95ff32ea89957c52130db8a
Benjamin Lis <benjaminlis@Benjamins-iMac.local>
2024-02-28 16:35:03 -0500

    Removed Waterston video per Matt

--- pages/2_Overview.py
+++ pages/2_Overview.py
@@ -61,9 +61,4 @@ st.subheader("Description")
 with open(c.config["corpora_description"], "r") as f:
     st.markdown(f.read())
 
-st.subheader("Motivation")
-st.markdown("Although this video is a few years old, it still does a great job\
-             explaining why we built and continue to expand the FOIArchive.")
-st.video("https://www.youtube.com/watch?v=6oTNw2jZ3zw&t=42s")
-
 boilerplate.footer()
