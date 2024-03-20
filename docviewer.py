3a8eedf10f0a02b93699446d0ff858de37ca2fc8
Benjamin Lis <benjaminlis@Benjamins-iMac.local>
2024-03-20 14:44:03 -0400

    Add display_body() and display_body_frus()

--- docviewer.py
+++ docviewer.py
@@ -6,6 +6,7 @@ import datetime
 import sqlgen as sg
 import db
 
+
 def display_date(date):
     if date:
         if date.time() == datetime.time(0, 0):
@@ -31,6 +32,20 @@ def display_source(source):
     if source:
         st.markdown(f"[Source Document PDF]({source})")
 
+def display_frus_body(id):
+    qry =(f"select body from docs_frus where doc_id = '{id}'")
+    results = db.execute(qry)
+    st.markdown(results.iloc[0]['body'], unsafe_allow_html=True)
+
+def display_body(doc):
+    if doc.body:
+        if doc.corpus == 'frus':
+            display_frus_body(doc.doc_id)
+        else:
+            st.write(doc.body)
+    else:
+        st.write("No document body")
+ 
 def display_cnt(type, cnt):
     if cnt:
         st.sidebar.markdown(f"### {type}: {cnt}")
@@ -39,7 +54,7 @@ def display_doc(doc):
     st.subheader(doc.title)
     display_date(doc.authored)
     display_source(doc.source)
-    st.write(doc.body)
+    display_body(doc)
     display_citation(doc.title, doc.corpus, doc.doc_id)
     st.sidebar.markdown(f"### Original Classification: {doc.classification}") 
     display_topics(doc.doc_id)
