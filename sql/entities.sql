41d6770bd84a62a2ee013136c18d71f7c80c40e1
Ben Lis <bl2759@columbia.edu>
2024-07-19 08:33:55 -0400

    Removed instance_cnt column

--- sql/entities.sql
+++ sql/entities.sql
@@ -1,4 +1,4 @@
-select entity_id, entity, entgroup, wikidata_id, doc_cnt, instance_cnt,
+select entity_id, entity, entgroup, wikidata_id, doc_cnt,
        case when wikidata_id is null then entity || ' (' || doc_cnt || ')' 
             else entity || ' (' || doc_cnt || ', '|| wikidata_id || ')' 
        end entity_dropdown_str
