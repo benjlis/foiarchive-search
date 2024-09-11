07799f56489fc23c78bce58eb7541dce2be1d67b
Ben Lis <bl2759@columbia.edu>
2024-09-11 10:20:29 -0400

    Changed order by to doc_cnt desc

--- sql/entities.sql
+++ sql/entities.sql
@@ -1,4 +1,4 @@
 select entity_id, entity, entgroup, wikidata_id, doc_cnt, 
        entity || ' (' || doc_cnt || ')' entity_dropdown_str
     from entities
-    order by entity;
+    order by doc_cnt desc;
